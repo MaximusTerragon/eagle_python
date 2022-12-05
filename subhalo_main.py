7b96f29bfd978e83729e9d98357e0575c8d5665e
MaximusTerragon <34456298+MaximusTerragon@users.noreply.github.com>
2022-12-05 14:12:15 +0000

    Subhalo_Extract units done
    - stelmass, gasmass unchanged
    - halfmass_rad, centre_centre_mass, coords now in kpc
    - perc_vel, velocity now in kms

--- subhalo_main.py
+++ subhalo_main.py
@@ -10,13 +10,11 @@ import matplotlib.colors as colors
 import matplotlib.cm as cm
 import eagleSqlTools as sql
 from mpl_toolkits.mplot3d import Axes3D
-
 from pafit.fit_kinematic_pa import fit_kinematic_pa
 from plotbin.sauron_colormap import register_sauron_colormap
 from vorbin.voronoi_2d_binning import voronoi_2d_binning
 from pyread_eagle import EagleSnapshot
-
-#from read_dataset_tools import read_dataset, read_dataset_dm_mass, read_header
+from read_dataset_tools import read_dataset, read_dataset_dm_mass, read_header
 from graphformat import graphformat
 
 
@@ -24,33 +22,42 @@ from graphformat import graphformat
 dataDir = '/Users/c22048063/Documents/EAGLE/data/RefL0012N0188/snapshot_028_z000p000/snap_028_z000p000.0.hdf5'
 
 
-
-
+"""
+Will convert all particle data to Msun, pkpc, s
+Boxsize and load_region_length in cMpc/h (I think) 
+"""
 class Subhalo_Extract:
     
-    def __init__(self, sim, gn, sgn, load_region_length=2):       # cMpc/h
+    def __init__(self, sim, data_dir, gn, sgn, centre_galaxy=True, load_region_length=2):       # cMpc/h
         # Assigning subhalo properties
         self.gn           = gn
         self.sgn          = sgn
         
         # Load information from the header
-        self.a, self.h, self.boxsize = read_header() # units of scale factor, h, and L [cMpc/h]        ASSUMES Z=0 I THINK
+        self.a, self.h, self.boxsize = read_header(data_dir) # units of scale factor, h, and L [cMpc/h]    ASSUMES Z=0 I THINK
         
         # For a given gn and sgn, run sql query on SubFind catalogue
         myData = self.query(sim)
         
         # Assiging subhalo properties
-        self.stelmass     = myData['stelmass']                                                              # [Msun]
-        self.gasmass      = myData['gasmass']                                                               # [Msun]
-        self.halfmass_rad = myData['rad']                                                                   # [pkpc]
-        self.centre       = 1000*np.array([myData['x'], myData['y'], myData['z']])                          # [pkpc]
-        self.centre_mass  = 1000*np.array([myData['x_mass'], myData['y_mass'], myData['z_mass']])           # [pkpc]
-        self.perc_vel     = np.array([myData['vel_x'], myData['vel_y'], myData['vel_z']]) * u.km.to(u.kpc)  # [pMpc/s]
+        self.stelmass     = myData['stelmass']                                                                  # [Msun]
+        self.gasmass      = myData['gasmass']                                                                   # [Msun]
+        self.halfmass_rad = myData['rad']                                                                       # [pkpc]
+        self.centre       = np.array([myData['x'], myData['y'], myData['z']]) * u.Mpc.to(u.kpc)                 # [pkpc]
+        self.centre_mass  = np.array([myData['x_mass'], myData['y_mass'], myData['z_mass']]) * u.Mpc.to(u.kpc)  # [pkpc]
+        self.perc_vel     = np.array([myData['vel_x'], myData['vel_y'], myData['vel_z']])                       # [pkpc/s]
         
         # Load data for stars and gas in non-centred units
-        self.stars        = self.read_galaxy(4, gn, sgn, self.centre, load_region_length)
-        self.gas          = self.read_galaxy(0, gn, sgn, self.centre, load_region_length)
+        # Msun, pkpc, and pkpc/s
+        self.stars        = self.read_galaxy(data_dir, 4, gn, sgn, self.centre*u.kpc.to(u.Mpc), load_region_length) 
+        self.gas          = self.read_galaxy(data_dir, 0, gn, sgn, self.centre*u.kpc.to(u.Mpc), load_region_length)
         
+        if centre_galaxy == True:
+            self.stars['Coordinates'] = self.stars['Coordinates'] - self.centre
+            self.gas['Coordinates']   = self.gas['Coordinates'] - self.centre
+            self.stars['Velocity'] = self.stars['Velocity'] - self.perc_vel
+            self.gas['Velocity']   = self.gas['Velocity'] - self.perc_vel
+
 
     def query(self, sim):
         # This uses the eagleSqlTools module to connect to the database with your username and password.
@@ -89,7 +96,7 @@ class Subhalo_Extract:
             return myData
             
         
-    def read_galaxy(self, itype, gn, sgn, centre, load_region_length):
+    def read_galaxy(self, data_dir, itype, gn, sgn, centre, load_region_length):
         """ For a given galaxy (defined by its GroupNumber and SubGroupNumber)
         extract the coordinates, velocty, and mass of all particles of a selected type.
         Coordinates are then wrapped around the centre to account for periodicity."""
@@ -98,7 +105,7 @@ class Subhalo_Extract:
         data = {}
         
         # Initialize read_eagle module.
-        eagle_data = EagleSnapshot(dataDir)
+        eagle_data = EagleSnapshot(data_dir)
         
         # Put centre into pMpc -> cMpc/h units.
         centre_cMpc = centre*self.h
@@ -108,7 +115,7 @@ class Subhalo_Extract:
             (centre_cMpc[0]-0.5*load_region_length), (centre_cMpc[0]+0.5*load_region_length),
             (centre_cMpc[1]-0.5*load_region_length), (centre_cMpc[1]+0.5*load_region_length),
             (centre_cMpc[2]-0.5*load_region_length), (centre_cMpc[2]+0.5*load_region_length)
-        ]) 
+        ])
         eagle_data.select_region(*region)
                 
         # Load data using read_eagle, load conversion factors manually.
@@ -144,9 +151,14 @@ class Subhalo_Extract:
         if itype == 0:
             data['StarFormationRate'] = data['StarFormationRate'] * u.g.to(u.Msun)  # [Msun/s]
         
-        # Periodic wrap coordinates around centre (in proper units). boxsize converted from cMpc/h -> pMpc
-        boxsize = self.boxsize/self.h       # [pMpc]
+        # Periodic wrap coordinates around centre (in proper units). 
+        # boxsize converted from cMpc/h -> pMpc
+        boxsize = self.boxsize/self.h       # [pkpc]
         data['Coordinates'] = np.mod(data['Coordinates']-centre+0.5*boxsize, boxsize) + centre-0.5*boxsize
+        
+        # Converting to pkpc
+        data['Coordinates'] = data['Coordinates'] * u.Mpc.to(u.kpc)     # [pkpc]
+        data['Velocity'] = data['Velocity'] * u.Mpc.to(u.km)            # [pkm/s]
         
         return data
         
@@ -156,4 +168,25 @@ class Subhalo_Extract:
         
         
         
+        
+
+        
 register_sauron_colormap()
+
+# list of simulations
+mySims = np.array([('RefL0012N0188', 12)])  
+
+GroupNum = 4
+SubGroupNum = 0
+
+# Initial extraction of galaxy data
+galaxy = Subhalo_Extract(mySims, dataDir, GroupNum, SubGroupNum)
+
+
+
+
+
+
+
+
+
